dbe5b04e6639cfbde6ae69edee90046059e7b9bd
Alex <github email address>
2017-09-08 13:00:20 -0400

    fix for interacting with console, class was accidentally upercased in CLI.rb

--- lib/wru/cli.rb
+++ lib/wru/cli.rb
@@ -1,10 +1,14 @@
 #this is our cli controller - responsible for user interactions in the bin folder daily--deal
 
-Class Wru::CLI
+class Wru::CLI
     def call
         list_matches
     end
     def list_matches
-      puts "These are matches"      
+      puts "These are matches"
     end
 end
+
+
+
+#     ./bin/daily-deal

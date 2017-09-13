ce28b55cf0154d81a6297223419e51ea13aa7350
Alex <github email address>
2017-09-13 12:23:30 -0400

    fixing scraper

--- lib/wru/match.rb
+++ lib/wru/match.rb
@@ -3,6 +3,7 @@ class Wru::Match
   attr_accessor :date, :time, :comp, :home, :score, :away, :venue, :more_info
   #lets keep track of a class array of all matches
 
+
   def self.upcoming
     puts <<-DOC.gsub /^\s*/, ''
               1. Match WRU vs X
@@ -16,6 +17,7 @@ class Wru::Match
   def get_page
     #set all html data from nokogirir into a variable
     doc = Nokogiri::HTML(open("http://www.wru.co.uk/eng/matchcentre/index.php"))
+
   end
   def get_matches
   # this code gives aan array of all matches on page
@@ -23,8 +25,10 @@ class Wru::Match
   end
   #take the array and enumerate on each
   def make_matches
-      self.get_matches.each do |event|
+    self.get_matches.each do |event|
+        matches = []
         match = Wru::Match.new
+        binding.pry
         match.date = event.css(".field_DateAbbrev").text
         match.time = event.css(".field_TimeLong").text
         match.comp = event.css(".field_CompStageAbbrev").text
@@ -34,9 +38,10 @@ class Wru::Match
         match.venue = event.css(".field_VenName").text
         #I am having a difficult time isolating the link url in match.more_info
         match.more_info = event.css(".field_Links").text
+        matches << match
 
-        match.venue
       end
+
   end
 
 

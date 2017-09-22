99e75c9598e49a1d411cf04f308807d7a1df5824
Alex <github email address>
2017-09-22 16:31:09 -0400

    finished cli before UAT testing (my wife)

--- lib/wru/cli.rb
+++ lib/wru/cli.rb
@@ -16,6 +16,10 @@ class Wru::CLI
     end
     def list_matches
       @matches = Wru::Schedule.new.make_matches
+      match_breakdown
+
+    end
+    def match_breakdown
       @matches.each.with_index(1) do |match, i|  #.with_index(1) is a trick to chain with_index(1) allows you to circumvent having to subtract one from the index of an array to yield its position ((arrays start at 0 this allows it to start at 1))
           puts "##{i}. #{match.date} - #{match.home} v #{match.away}"
       #@deals.each.with_index(1) do |deal, i|
@@ -23,15 +27,17 @@ class Wru::CLI
       end
       puts #space line
       puts  "Enter the number of the match would you like to learn more information about."
-
+      puts #space
       #in this section lets call a class that extracts match info
-
     end
 
+
     def schedule
+
         input =nil
         while input != "exit"
               input = gets.strip.downcase #downcase allows any input to be processed in a similar manner
+              puts #space
               if input.to_i > 0 #strings convert to 0 value with .to_i
 
                         the_match = @matches[input.to_i - 1] #this provides the array index of this match and then puts the object
@@ -46,12 +52,16 @@ class Wru::CLI
                               DOC
                         puts
                         puts <<-DOC.gsub /^\s*/, ''
-                                There are #{@matches.count} matches in the schedule. Type the corresponding number from the list of upcoming matches to learn more.  If you would like to exit, type "exit" into the terminal.
+                                There are #{@matches.count} matches in the schedule. Type the corresponding match number to learn more, type list to see the schedule again, or type exit if you would like to stop the program.
                              DOC
+              elsif input == "list"
+                  match_breakdown
+              elsif input == "exit"
+                puts "Thanks for visiting. Go CYMRU!!"
               else
-                puts "Not sure what you want with your last input. Type the corresponding match number to learn more or type exit if you would like to stop the program."
+                puts "Not sure what you want with your last input. Type the corresponding match number to learn more, type list to see the schedule again, or type exit if you would like to stop the program."
                 puts
-                list_matches
+                match_breakdown
               end
         end
     end

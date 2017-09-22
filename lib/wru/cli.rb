80e51296d883887bb23cf44ea857ddf1d8e15dcd
Alex <github email address>
2017-09-22 07:30:06 -0400

    cosmetic touch up to cli

--- lib/wru/cli.rb
+++ lib/wru/cli.rb
@@ -13,7 +13,7 @@ class Wru::CLI
     #  binding.pry
       @matches = Wru::Schedule.new.make_matches
       @matches.each.with_index(1) do |match, i|  #.with_index(1) is a trick to chain with_index(1) allows you to circumvent having to subtract one from the index of an array to yield its position ((arrays start at 0 this allows it to start at 1))
-          puts "#{i}. #{match.date} - #{match.home} v #{match.away}"
+          puts "##{i}. #{match.date} - #{match.home} v #{match.away}"
       #@deals.each.with_index(1) do |deal, i|
       #    puts "#{i}. #{deal.name} - #{deal.price} - #{deal.availability}"
       end
@@ -28,6 +28,7 @@ class Wru::CLI
         while input != "exit"
               input = gets.strip.downcase #downcase allows any input to be processed in a similar manner
               if input.to_i > 0 #strings convert to 0 value with .to_i
+
                         the_match = @matches[input.to_i - 1] #this provides the array index of this match and then puts the object
                         puts <<-DOC.gsub /^\s*/, ''
                               Date = #{the_match.date}
@@ -38,8 +39,12 @@ class Wru::CLI
                               Venue = #{the_match.venue}
                               Additional Information = #{the_match.more_info}
                               DOC
+                        puts
+                        puts <<-DOC.gsub /^\s*/, ''
+                                There are #{@matches.count} matches in the schedule. Type the corresponding number from the list of upcoming matches to learn more.  If you would like to exit, type "exit" into the terminal.
+                             DOC
               elsif
-                list_deals
+                list_matches
               else
                 puts "Not sure what you want, type list or exit."
               end

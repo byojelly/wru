1fedabbf7e642c56e148699725acdeb821b12c65
Alex <github email address>
2017-10-08 17:34:06 -0400

    CernanB functionality included

--- lib/wru/match.rb
+++ lib/wru/match.rb
@@ -22,38 +22,24 @@ class Wru::Match
     @@matches
   end
 
+
+
   def self.matches_with(team)
       chosen_team = []
+      team = team.downcase
       Wru::Match.matches.each do |game|
            if game.away.downcase.include?(team)
             chosen_team << game
           elsif game.home.downcase.include?(team)
             chosen_team << game
            end
-
        end
        chosen_team
-
-
-      #      collect do |game, team|
-
-      #     if game.away.downcase.include?("italy")
-      #      chosen_team << game
-      #     elsif game.home.downcase.include?("italy")
-      #      chosen_team << game
-      #     end
-      #      chosen_team[0]
-      #     end
 
     #returns an array of all the match objects with the team passed in as an argument
     ##when done, push to github, then send info to reviewer on slack slack id to cernanb
   end
 end
-
-# =>
-#          cd code
-#          cd labs
-#          cd wru
 
   # =>    ./bin/schedule
 #         Wru::Match.matches_with("italy")
